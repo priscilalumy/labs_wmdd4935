df8b325a7758a3ffac05a80c4cfec58acb32a0df
priscilalumy <priscilalumy@gmail.com>
2017-12-03 11:21:08 +0000

    update repos 3

--- ct6_arrowfunctions/index.js
+++
@@ -1,4 +0,0 @@
-let inputs = process.argv.slice(2);
-let result = inputs.map(x => x[0])
-                    .reduce((charac, x) => charac + x);
-console.log(`[${inputs}] becomes "${result}"`);
--- ct6_destructuring/index.js
+++
@@ -1,4 +0,0 @@
-let userArray = process.argv.slice(2);
-let myResult = {};
-[, myResult.username, myResult.email] = userArray;
-console.log(myResult);
